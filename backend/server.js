4f48fbb564d6c232ad26ae04a23b25fd7e688a76
Ashiko124 <jonathangreene888@gmail.com>
2018-12-18 22:48:26 -0700

    Heroku test 3

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const express = require('express');
 
 
 // set the port to run the server on.
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000;
 const server = express();
 
 // get mongoose to use global promise library. 
@@ -34,7 +34,7 @@ server.get('/', (req, res) => res.send("The server is up and running!"));
 
 
 //initializing the server
-server.listen(port, () => console.log('The server is listening on port: ' + port));
+server.listen(port, (req, res) => console.log('The server is listening on port: ' + port));
 
 
-module.exports = server;
+//module.exports = server;
